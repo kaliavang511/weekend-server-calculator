701ee6c7e01ef5bd4765f7413681a756064a1214
Kalia Vang <kaliavang511@gmail.com>
2024-05-13 09:11:10 -0500

    frontend working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,56 @@
-console.log('client.js is sourced!');
+
+function handleSubmit(event){
+    event.preventDefault()
+
+
+    
+    let firstNum = parseInt(document.getElementById('firstNumInput').value)
+    let secNum = parseInt(document.getElementById('secNumInput').value)
+    
+    let addButton = document.getElementById('equals')
+    addButton = firstNum + secNum
+    
+    console.log(addButton)
+
+
+
+
+
+let adding = document.getElementById('history')
+adding.innerHTML +=`
+<span> ${firstNum} + ${secNum} = ${addButton} </span>
+`
+
+let subtrackButton = document.getElementById('subtrack')
+subtrackButton = parseInt(firstNum )- parseInt(secNum)
+let subtracking = document.getElementById('history')
+subtracking.innerHTML +=`
+<span> ${firstNum} - ${secNum} = ${subtrackButton} </span>
+`
+
+
+
+
+let multiplyButton = document.getElementById('multiply')
+multiplyButton = parseInt(firstNum )* parseInt(secNum)
+let mutiplying = document.getElementById('history')
+mutiplying.innerHTML +=`
+<span> ${firstNum} * ${secNum} = ${multiplyButton} </span>
+`
+
+
+
+let divideButton = document.getElementById('divide')
+divideButton = parseInt(firstNum )/ parseInt(secNum)
+let dividing = document.getElementById('history')
+dividing.innerHTML +=`
+<span> ${firstNum} / ${secNum} = ${divideButton} </span>
+`
+
+
+
+
+
+
+
+}
